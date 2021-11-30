8963b8c74c91b8bda1b684d1cb9b8bcde207b491
Jiin <jiinkim02@gmail.com>
2021-11-29 23:38:38 -0800

    fix asset issue and fix logic for calculating days

--- src/sky/Sky.tsx
+++ src/sky/Sky.tsx
@@ -1,23 +1,19 @@
 import { useState, useEffect } from "react";
-import night from "../assets/night.jpg";
-import sunrise from "../assets/sunrise.jpg";
-import day from "../assets/t.jpg";
-
-// import daysmall from "../assets/daysmall.jpg"
-// import daymed from "../assets/daymed.jpg"
-// import daybig from "../assets/daybig.jpg"
-// import sunsmall from "../assets/sunsmall.jpg"
-// import sunmed from "../assets/sunmed.jpg"
-// import sunbig from "../assets/sunbig.jpg"
-// import nightsmall from "../assets/nightsmall.jpg"
-// import nightmed from "../assets/nightmed.jpg"
-// import nightbig from "../assets/nightbig.jpg"
+import daysmall from "../assets/daysmall.jpg";
+import daymed from "../assets/daymed.jpg";
+import daybig from "../assets/daybig.jpg";
+import sunsmall from "../assets/sunsmall.jpg";
+import sunmed from "../assets/sunmed.jpg";
+import sunbig from "../assets/sunbig.jpg";
+import nightsmall from "../assets/nightsmall.jpg";
+import nightmed from "../assets/nightmed.jpg";
+import nightbig from "../assets/nightbig.jpg";
 
 import "./sky.css";
 
 export default function Sky() {
   const [date, setDate] = useState(new Date());
-  const [lastLogged, setLastLogged] = useState(new Date());
+  const [lastLogged] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -30,27 +26,27 @@ export default function Sky() {
   }, []);
 
   const hour = date.getHours();
-  var days = date.getTime() - lastLogged.getTime() / (1000 * 3600 * 24);
+  var days = (date.getTime() - lastLogged.getTime()) / (1000 * 3600 * 24);
 
   const image = () => {
-    if (hour >= 19 || hour <= 4){
-      if(days<2) return nightbig;
-      else if (days<5) return nightmed;
+    console.log(days);
+    if (hour >= 19 || hour <= 4) {
+      if (days < 2) return nightbig;
+      else if (days < 5) return nightmed;
       return nightsmall;
-    } 
+    }
     if (hour >= 5 && hour <= 6) {
-      if(days<2) return sunbig;
-      else if (days<5) return sunmed;
+      if (days < 2) return sunbig;
+      else if (days < 5) return sunmed;
       return sunsmall;
     }
     if (hour >= 7 && hour <= 16) {
-      if(days<2) return daybig;
-      else if (days<5) return daymed;
+      if (days < 2) return daybig;
+      else if (days < 5) return daymed;
       return daysmall;
-    }
-    else {
-      if(days<2) return sunbig;
-      else if (days<5) return sunmed;
+    } else {
+      if (days < 2) return sunbig;
+      else if (days < 5) return sunmed;
       return sunsmall;
     }
   };
