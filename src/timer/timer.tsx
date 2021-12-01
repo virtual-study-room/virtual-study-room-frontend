737388a5a2c9c5066ab36a5b4ad81e34c9876b81
Matthew Nieva <matthewcn56@gmail.com>
2021-11-30 16:59:56 -0800

    changed tick back to minutes

--- src/timer/timer.tsx
+++ src/timer/timer.tsx
@@ -11,22 +11,26 @@ interface timerProps {
 export default function Timer(props: timerProps): JSX.Element {
   const { handleEnd } = useContext(TimerContext);
 
-  const [[hrs, mins], setTime] = useState<[number, number]>([0,-1]);
+  const [[hrs, mins], setTime] = useState<[number, number]>([0, -1]);
   const [alerting, setAlerting] = useState(false);
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     if (localStorage.getItem(user?.username + " hours") !== null) {
-        console.log("mount");
-        const newHrs = parseInt(localStorage.getItem(user?.username + " hours")??"0");
-        const newMins = parseInt(localStorage.getItem(user?.username + " mins")??"-1");
-        setTime([newHrs,newMins]);
-        console.log([newHrs, newMins])
+      console.log("mount");
+      const newHrs = parseInt(
+        localStorage.getItem(user?.username + " hours") ?? "0"
+      );
+      const newMins = parseInt(
+        localStorage.getItem(user?.username + " mins") ?? "-1"
+      );
+      setTime([newHrs, newMins]);
+      console.log([newHrs, newMins]);
     } else {
-        setAlerting(false)
-        setTime([props.time[0],props.time[1]]);
+      setAlerting(false);
+      setTime([props.time[0], props.time[1]]);
     }
-  },[user,props.time]) //only when mounted or user changes
+  }, [user, props.time]); //only when mounted or user changes
 
   const tick = () => {
     console.log("ticktick");
@@ -34,14 +38,14 @@ export default function Timer(props: timerProps): JSX.Element {
     //set new time in local storage
     if (hrs > 0 && mins === 0) {
       setTime(([hrs, mins]) => [hrs - 1, 59]);
-      localStorage.setItem(user?.username + " hours",(hrs-1).toString());
-      console.log("set local storage hours to: " + (hrs-1));
-      localStorage.setItem(user?.username + " mins","59");
+      localStorage.setItem(user?.username + " hours", (hrs - 1).toString());
+      console.log("set local storage hours to: " + (hrs - 1));
+      localStorage.setItem(user?.username + " mins", "59");
     } else if (mins > 0) {
       //console.log("entered yuh");
       setTime(([hrs, mins]) => [hrs, mins - 1]);
-      localStorage.setItem(user?.username + " hours",hrs.toString());
-      localStorage.setItem(user?.username + " mins",(mins-1).toString());
+      localStorage.setItem(user?.username + " hours", hrs.toString());
+      localStorage.setItem(user?.username + " mins", (mins - 1).toString());
     }
   };
 
@@ -56,7 +60,7 @@ export default function Timer(props: timerProps): JSX.Element {
   //change time
   useEffect(() => {
     console.log("Started ticking!");
-    const timerId = setInterval(() => tick(), 6000);
+    const timerId = setInterval(() => tick(), 60000);
     return () => clearInterval(timerId);
     // eslint-disable-next-line
   }, [hrs, mins]);
