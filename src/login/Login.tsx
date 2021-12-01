b55e7de002c6a7662d6cc7e4a3d1327428e3e68c
Jiin <jiinkim02@gmail.com>
2021-11-30 21:38:30 -0800

    press enter to log in

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import { AuthContext } from "../auth/AuthContext";
 import React, { useContext, useState } from "react";
-import "./Login.css"
-import "../styles/App.css"
+import "./Login.css";
+import "../styles/App.css";
 
 export default function Login() {
   const { attemptLogin } = useContext(AuthContext);
@@ -12,28 +12,37 @@ export default function Login() {
   return (
     <div className="login">
       <div className="login-container">
-      <div>
-        <div className="login-text">username: </div>
-        <input className="input"
-          type="text"
-          value={userInput}
-          onChange={(event) => setUserInput(event.target.value)}
-        />
+        <div>
+          <div className="login-text">username: </div>
+          <input
+            className="input"
+            type="text"
+            value={userInput}
+            onChange={(event) => setUserInput(event.target.value)}
+          />
+        </div>
+        <div>
+          <div className="login-text">password:</div>
+          <input
+            className="input"
+            type="password"
+            value={passwordInput}
+            onChange={(event) => setPasswordInput(event.target.value)}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") attemptLogin(userInput, passwordInput);
+            }}
+          />
+        </div>
+        <button
+          className="button"
+          onClick={() => attemptLogin(userInput, passwordInput)}
+        >
+          Log In
+        </button>
+        <Link className="button action" to="/register">
+          Register
+        </Link>
       </div>
-      <div>
-        <div className="login-text">password:</div>
-        <input className="input"
-          type="password"
-          value={passwordInput}
-          onChange={(event) => setPasswordInput(event.target.value)}
-        />
-      </div>
-      <button className="button" onClick={() => attemptLogin(userInput, passwordInput)}>
-        Log In
-      </button>
-      <Link className="button action" to="/register">Register</Link>
-      </div>
-      
     </div>
   );
 }
